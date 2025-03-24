1eb648ac297443688182c3f21481ae56e5ca071d
DannyGoblin <forgottenpinkie@icloud.com>
2025-03-24 09:41:00 -0700

    Settings/Sound added

---
+++ Assets/Scripts/VolumeControl.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+//Attach script to empty gameobject
+public class VolumeControl : MonoBehaviour
+{
+    public Slider sfxSlider;
+    public Slider musicSlider;
+    public AudioMixer sfxMixer;
+    public AudioMixer musicMixer;
+
+    public string sfxVolumeParameter = "SFXVolume";
+    public string musicVolumeParameter = "MusicVolume";
+    void Start()
+    {
+        float sfxValue;
+        float musicValue;
+
+        sfxMixer.GetFloat(sfxVolumeParameter, out sfxValue);
+        musicMixer.GetFloat(musicVolumeParameter, out musicValue);
+
+        sfxSlider.value = sfxValue;
+        musicSlider.value = musicValue;
+
+        sfxSlider.onValueChanged.AddListener(SetSFXVolume);
+        musicSlider.onValueChanged.AddListener(SetMusicVolume);
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        sfxMixer.SetFloat(sfxVolumeParameter, volume);
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        musicMixer.SetFloat(musicVolumeParameter, volume);
+    }
+}
